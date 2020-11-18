6194c4c293dd35646cae4a7d7d9f20e8ce77cf53
Deborah de Deus Tuba <deborahtuba@gmail.com>
2020-11-17 21:49:54 -0300

    escrevendo head para passar um status code e tipo de conta

--- app.js
+++ app.js
@@ -2,5 +2,9 @@ const http = require('http')
 const { listenerCount } = require('process')
 
 http.createServer((req, res) => {
-    res.end('Servidor funfando!')
+    res.writeHead(200, {
+        'Content-Type': 'text/plain'
+    })
+    console.log('Servidor rodando corretamente')
+    res.end('Servidor funcionando novamente! De um jeitinho mais correto')
 }).listen(3030, 'localhost')
