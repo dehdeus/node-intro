4c0619e7f6d0280a1d331e5b63e8afdb6a9f8d86
Deborah de Deus Tuba <deborahtuba@gmail.com>
2020-11-17 21:55:09 -0300

    agora passando via json

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const { listenerCount } = require('process')
 
 http.createServer((req, res) => {
     res.writeHead(200, {
-        'Content-Type': 'HTML'
+        'Content-Type': 'application ld/json'
     })
     console.log('Servidor rodando corretamente')
-    res.end('<h1>Servidor rodando!</h1><h2>Uhhullll</h2>')
+    res.end('[{"nome":"Fulano","sobrenome":"Silva"},{"nome":"Ciclano","sobrenome":"Silva"},{"nome":"Beltrano","sobrenome":"Silva"}]')
 }).listen(3030, 'localhost')
