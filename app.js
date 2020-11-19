4990bbfa2e36d90e3e9b24a845a859a176a83d1d
Deborah de Deus Tuba <deborahtuba@gmail.com>
2020-11-19 20:20:45 -0300

    feature: aplicando rotas no nodeja

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const http = require('http')
-const { listenerCount } = require('process')
 
 http.createServer((req, res) => {
     res.writeHead(200, {
-        'Content-Type': 'application ld/json'
+        'Content-Type': 'text/plain; charset=utf-8'
     })
     console.log('Servidor rodando corretamente')
-    res.end('[{"nome":"Fulano","sobrenome":"Silva"},{"nome":"Ciclano","sobrenome":"Silva"},{"nome":"Beltrano","sobrenome":"Silva"}]')
+    switch (req.url) {
+        case '/':
+            res.end('Olá! Você está na página Inicial')
+            break
+        case '/sobre':
+            res.end('Olá! Você está na página Sobre')
+            break
+        case '/produtos':
+            res.end('Olá! Você está na página Produtos')
+            break
+        default:
+            res.end(`Ops! Parece que não encontramos a página ${req.url.replace('/', '')}`)
+            break
+    }
 }).listen(3030, 'localhost')
+    
